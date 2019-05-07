958e7880e5718d4dfe2033e771d14bfffdfdcb70
adrianjcorrea <adrianjcorrea007@gmail.com>
2019-05-07 17:02:56 -0500

    Added logic to IslandCount

--- IslandCount.js
+++ IslandCount.js
@@ -47,8 +47,36 @@ function create2DMatrix(strMap){
     //Splitting each row at index and pushing that returned array to matrix array.
     matrix.push(row.split(''))
   )
-  //returnin matrix;
-  return matrix;
+
+  //This function defines border of matrix
+  function dfs(i, j, matrix) {
+    //If it gets to edge of matrix it will change i and j value ( Changing starting point location)
+    if (i < 0 || j < 0 || i >= matrix.length || j >= matrix[0].length || matrix[i][j] != '0')
+     return;
+       //Changes value of indexes to * since it has already been iterated thru and its checking if indexes are value 0.
+       matrix[i][j] = '*';
+       dfs(i-1, j, matrix);
+       dfs(i+1, j, matrix);
+       dfs(i, j-1, matrix);
+       dfs(i, j+1, matrix);
+  }
+  //This is my counter
+  let counter = 0;
+
+  //Nested looping if
+  for (let i = 0; i < strMap.length; i++) {
+    for (let j = 0; j < strMap[0].length; j++) {
+      //index in j position and i possition equal 0 incrementen counter by 1
+      if (strMap[i][j] === '0') {
+        counter += 1;
+        //Run next function call of dfs until all 0 are changed with *
+        dfs(i, j, strMap);
+      }
+    }
+  }
+  return counter;
 }
 
-create2DMatrix("..000.\n..000.\n..000.\n.0....\n..000.")
+create2DMatrix("..000.\n..000.\n..000.\n.0....\n..000.");
+create2DMatrix("0...0\n..0..\n0...0");
+create2DMatrix("0....\n.00..\n....0");
